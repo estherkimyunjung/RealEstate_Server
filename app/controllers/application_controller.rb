2feaf200145f35973307f26299207c48ffff6299
estherkimyunjung <estherkimyunjung@gmail.com>
2020-07-19 02:24:38 -0500

    User Client Agent CRUD Done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,9 @@ class ApplicationController < ActionController::API
 
   def logged_in?
     headers = request.headers["Authorization"]
+    if !headers
+      return nil
+    end
     token = headers.split(" ")[1]
 
     begin
@@ -19,4 +22,50 @@ class ApplicationController < ActionController::API
     end
       render json: {error: "Please Login"} unless user
   end
+
+  # before_action :check_authentication
+    
+  # def encode_token(payload) # for token generation
+  #     JWT.encode(payload, "RealEstate1800", "HS256") #algo is optional as a default HS256
+  # end
+
+  # def auth_header
+  #   # { 'Authorization': 'Bearer <token>' }
+  #   request.headers['Authorization']
+  #   # byebug
+  # end
+
+  # def current_user
+  #   if decoded_token
+  #     user_id = decoded_token["user_id"]
+  #     @user = User.find(user_id)
+  #   end
+  # end
+  
+  # def decoded_token
+  #     # token => "eyJhbGciOiJIUzI1NiJ9.eyJiZWVmIjoic3RlYWsifQ._IBTHTLGX35ZJWTCcY30tLmwU9arwdpNVxtVU0NpAuI" 
+  #   if !auth_header
+  #     return nil
+  #   end
+
+  #   begin
+  #     token = auth_header.split(' ')[1]
+  #     JWT.decode(token,"RealEstate1800")[0] #pass the same key
+  #     # JWT.decode => [{ "user_id"=>"18" }, { "alg"=>"HS256" }]
+  #     # [0] gives us the payload { "user_id"=>"18" }
+  #   rescue JWT::DecodeError
+  #     nil
+  #   end
+  # end
+  
+  # def check_authentication
+  #   render json: { error: 'Please log in' }, status: 401 if !logged_in?
+  # end
+  
+  # def logged_in?
+  #   # byebug
+  #   !!current_user
+  # end
 end
+
+
